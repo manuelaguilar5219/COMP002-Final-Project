706f87322999e236bf0b38882826fd5cff72f845
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-12-03 18:08:20 -0800

    added semicolon's that I forgot to add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentPlayer = 'X'; // The initial player is set to X
 let gameActive = true; // Indicates if the game is responsive and is accepting moves
-let gameState = ['', '', '', '', '', '', '', '', '',] // Made an Array that keeps track of the board state
+let gameState = ['', '', '', '', '', '', '', '', '',]; // Made an Array that keeps track of the board state
 let scoreX = localStorage.getItem('scoreX') ? parseInt(localStorage.getItem('scoreX')) : 0; // Tracks the score form player X and retrieves it from local storage 
 let scoreO = localStorage.getItem('scoreO') ? parseInt(localStorage.getItem('scoreO')) : 0; // Tracks the score form player O and retrieves it from local storage
 
@@ -13,4 +13,4 @@ const winningConditions = [
     [2, 5, 8], // Set the winning conditions for the third column
     [0, 4, 8], // Set the winning conditions for the top left to bottom right diagonal
     [2, 4, 6], // Set the winning conditions for the top right to bottom left diagonal
-]
+];
